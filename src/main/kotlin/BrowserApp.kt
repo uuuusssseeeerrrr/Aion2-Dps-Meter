3af74ffafcbba2114d971084fae8ca609f5e0346
TK-open-public <itaebread2@gmail.com>
2026-01-09 13:42:36 +0900

    Feat: 웹뷰 브릿지 추가 및 오버레이 창이동 추가

--- src/main/kotlin/BrowserApp.kt
+++ src/main/kotlin/BrowserApp.kt
@@ -1,43 +1,75 @@
 package com.tbread
 
-import androidx.compose.desktop.ui.tooling.preview.Preview
-import androidx.compose.runtime.Composable
 import javafx.application.Application
+import javafx.concurrent.Worker
 import javafx.scene.Scene
+import javafx.scene.input.MouseEvent
+import javafx.scene.layout.Pane
+import javafx.scene.layout.StackPane
 import javafx.scene.paint.Color
 import javafx.scene.web.WebView
 import javafx.stage.Stage
 import javafx.stage.StageStyle
+import netscape.javascript.JSObject
 import java.io.File
 
 class BrowserApp : Application() {
 
+    class JSBridge(private val stage: Stage) {
+        fun moveWindow(x: Double, y: Double) {
+            stage.x = x
+            stage.y = y
+        }
+    }
+
+//    private var xOffset = 0.0
+//    private var yOffset = 0.0
 
     override fun start(stage: Stage) {
-        val webView = WebView()
-        val engine = webView.engine
-        engine.load(File("index.html").toURI().toString())
+            val webView = WebView()
+            val engine = webView.engine
+            engine.load(File("index.html").toURI().toString())
+//
+//            webView.addEventFilter(MouseEvent.MOUSE_PRESSED) { e ->
+//                xOffset = e.sceneX
+//                yOffset = e.sceneY
+//                e.consume()
+//            }
+//            webView.addEventFilter(MouseEvent.MOUSE_DRAGGED) { e ->
+//                stage.x = e.sceneX - xOffset
+//                stage.y = e.sceneY - yOffset
+//                e.consume()
+//            }
 
-        val scene = Scene(webView, 1000.0, 800.0)
-        scene.fill = Color.TRANSPARENT
-
-        try {
-            val pageField = engine.javaClass.getDeclaredField("page")
-            pageField.isAccessible = true
-            val page = pageField.get(engine)
-
-            val setBgMethod = page.javaClass.getMethod("setBackgroundColor", Int::class.javaPrimitiveType)
-            setBgMethod.isAccessible = true
-            setBgMethod.invoke(page, 0)
-        } catch (e: Exception) {
-            println("리플렉션 실패: ${e.message}")
+        val bridge = JSBridge(stage)
+        engine.loadWorker.stateProperty().addListener { _, _, newState ->
+            if (newState == Worker.State.SUCCEEDED) {
+                val window = engine.executeScript("window") as JSObject
+                window.setMember("javaBridge", bridge)
+            }
         }
 
-        stage.initStyle(StageStyle.TRANSPARENT)
-        stage.scene = scene
-        stage.isAlwaysOnTop = true
-        stage.title = "Aion2 Dps Overlay"
 
-        stage.show()
-    }
+            val scene = Scene(webView, 1000.0, 800.0)
+            scene.fill = Color.TRANSPARENT
+
+            try {
+                val pageField = engine.javaClass.getDeclaredField("page")
+                pageField.isAccessible = true
+                val page = pageField.get(engine)
+
+                val setBgMethod = page.javaClass.getMethod("setBackgroundColor", Int::class.javaPrimitiveType)
+                setBgMethod.isAccessible = true
+                setBgMethod.invoke(page, 0)
+            } catch (e: Exception) {
+                println("리플렉션 실패: ${e.message}")
+            }
+
+            stage.initStyle(StageStyle.TRANSPARENT)
+            stage.scene = scene
+            stage.isAlwaysOnTop = true
+            stage.title = "Aion2 Dps Overlay"
+
+            stage.show()
+        }
 }
