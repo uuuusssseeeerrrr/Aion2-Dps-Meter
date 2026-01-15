5daaf2500b71c1a2cecda85fbd379d9220065a1d
TK-open-public <itaebread2@gmail.com>
2026-01-15 17:30:10 +0900

    Feat: 모든직업 평타 스킬코드 추가

--- src/main/kotlin/entity/JobClass.kt
+++ src/main/kotlin/entity/JobClass.kt
@@ -2,13 +2,13 @@ package com.tbread.entity
 
 enum class JobClass(val className: String, val basicSkillCode: Int) {
     WARRIOR("검성", 9952),
-    PALADIN("수호성", 0),
+    PALADIN("수호성", 16912),
     RANGER("궁성", 60832),
     ROGUE("살성", 33872),
-    SORCERER("마도성", 0),
-    CLERIC("치유성", 0),
+    SORCERER("마도성", 5648), /* 마도 확인 필요함 */
+    CLERIC("치유성", 36176),
     SUMMONER("정령성", 19216),
-    SHAMAN("호법성", 0);
+    SHAMAN("호법성", 53136);
 
     companion object{
         fun convertFromSkill(skillCode:Int):JobClass?{
